8812a60b2077f9e7e1757d34355d9ae262d3c74e
APEX UniLaSalle <52005031+APEX-UniLaSalle@users.noreply.github.com>
2025-12-19 14:58:42 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -24,7 +24,8 @@ FILE_NAMES = [
     "xp_SkillQuest_2025-12-12.xlsx",
     "xp_SkillQuest_2025-12-15.xlsx",
     "xp_SkillQuest_2025-12-16.xlsx",
-    "xp_SkillQuest_2025-12-17.xlsx"
+    "xp_SkillQuest_2025-12-17.xlsx",
+    "xp_SkillQuest_2025-12-19.xlsx"
 ]
 
 # Constantes pour les niveaux et les couleurs
